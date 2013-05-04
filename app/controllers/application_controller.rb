2e16c056b12d8cc3abeff10bb6649256125b8aa6
Shalini <shalini.dalal@yahoo.com.au>
2013-05-04 18:06:04 +1000

    second commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, :alert => exception.message
+  private
+
+  	def current_cart
+  		Cart.find(session[:cart_id])
+  		rescue ActiveRecord::RecordNotFound
+  		cart = Cart.create
+  		#session[:cart_id] = cart.cart_id
+  		
+
+  #rescue_from CanCan::AccessDenied do |exception|
+    #redirect_to root_path, :alert => exception.message
   end
 
 end
